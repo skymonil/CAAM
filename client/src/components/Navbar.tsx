ee97d1d58b16ddb66c91dcde95fbc3055c5f42de
Chirag-varu <chiragvaru03@gmail.com>
2024-12-30 19:05:09 +0530

    Logout functnality added

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,9 @@
 import { User, LogOut } from "lucide-react";
 
+const handleLogout = () => {
+  window.location.href = "/log-in";
+}
+
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center bg-gray-800 text-white p-4">
@@ -7,7 +11,7 @@ const Navbar = () => {
         <User size={20} /> {/* User Icon next to name */}
         <div className="ml-2 text-lg font-semibold">Chirag Varu</div> {/* Name */}
       </div>
-      <button className="flex items-center cursor-pointer hover:bg-gray-700 p-2 rounded">
+      <button className="flex items-center cursor-pointer hover:bg-gray-700 p-2 rounded" onClick={handleLogout}>
         <LogOut size={20} /> {/* Logout Icon */}
         <span className="ml-2">Logout</span>
       </button>
--- client/src/pages/SuperAdmin/Navbar.tsx
+++ client/src/pages/SuperAdmin/Navbar.tsx
@@ -1,5 +1,9 @@
 import { User, LogOut } from "lucide-react";
 
+const handleLogout = () => {
+  window.location.href = "/log-in";
+}
+
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center bg-gray-800 text-white p-4">
@@ -8,7 +12,7 @@ const Navbar = () => {
         <div className="ml-2 text-lg font-semibold">Super Admin</div>{" "}
         {/* Name */}
       </div>
-      <button className="flex items-center cursor-pointer hover:bg-gray-700 p-2 rounded">
+      <button className="flex items-center cursor-pointer hover:bg-gray-700 p-2 rounded" onClick={handleLogout}>
         <LogOut size={20} /> {/* Logout Icon */}
         <span className="ml-2">Logout</span>
       </button>
