01d8ee7f117c52566da1960bf24775b15783b231
adityapai05 <adityapai.main@gmail.com>
2025-01-01 13:25:38 +0530

    bug fix

--- server/src/lib/utils.js
+++ server/src/lib/utils.js
@@ -9,6 +9,7 @@ export const generateToken = (userID, res) => {
   res.cookie("token", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
+    sameSite: "strict",
     secure: process.env.HTTPS,
   });
 
