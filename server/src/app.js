e31599c3a692ae37d5e0a23d2bb854be7d50c936
Akshat Gohil <akshatgohil4243@gmail.com>
2025-01-03 21:27:54 +0530

    Added Grievance Controller to Add, Resolve and Fetch Grievances

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,9 @@ import {connectDB} from './lib/db.js';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import authRoute from './routes/auth.route.js';
-import accountantRoute from './routes/accountant.route.js'
+import accountantRoute from './routes/accountant.route.js';
+import grievanceRoute from './routes/grievance.route.js';
+
 const app = express();
 const PORT = 5000;
 
@@ -20,7 +22,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api/auth', authRoute);
-app.use('/api/acc',accountantRoute);
+
+//Endpoint to Add Money to Student Wallet from Accountant Side
+app.use('/api/admin/accountant',accountantRoute);
+
+//Endpoint to Add Grievance from Student side and Approve Grievance from HOD side
+app.use('/api/grievance',grievanceRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on : ${PORT}`);
