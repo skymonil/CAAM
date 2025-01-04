fe9e02f29f58de02d24e2dfeb6c7770ecc51b80e
Chirag-varu <chiragvaru03@gmail.com>
2025-01-04 23:15:57 +0530

    Admin Detail funcnality added the data like username and role are comming from DB with api

--- server/src/controllers/admin.controller.js
+++ server/src/controllers/admin.controller.js
@@ -15,3 +15,30 @@ export const getAdmin = async (req, res) => {
         res.status(500).json({ message: "Server error", error });
     }
 };
+
+export const getAdmincredential = async (req, res) => {
+    try {
+        const AdminEmail = req.admin.email;
+        const collegeName = await Admin.findOne({ email: AdminEmail }).select("collegeName");
+
+        console.log(collegeName);
+        
+
+        if (!collegeName) {
+            return res.status(404).json({ message: "admin not found" });
+        }
+
+        const admins = await Admin.find({ collegeName: collegeName.collegeName }).select("username role password");
+
+        console.log(admins);
+
+        if (!admins || admins.length === 0) {
+            return res.status(404).json({ message: "No admins found for this college" });
+          }
+
+        res.status(200).json({ admins });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server error", error });
+    }
+};
