be93187f103d6979e9626d5c692365438ae1dfea
Chirag-varu <chiragvaru03@gmail.com>
2025-01-05 17:27:08 +0530

    app.tsx improved

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -55,45 +55,38 @@ const AppContent = () => {
 
   return (
     <>
-    <StudentProvider>
-      {shouldHaveNavbar && <Navbar />}
-      <Routes>
-        <Route path="/log-in" element={<LogIn />} />
-        <Route
-          path="/"
-          element={<Register />}
-          />
-        <Route
-          path="/admin-register"
-          element={<AdminRegister />}
-          />
-        <Route
-          path="/admin-login"
-          element={<AdminLogin />}
-          />
-        <Route path="/student-detail" element={<Dashboard />} />
-        <Route path="/admission/*" element={<Admission />} />
-        <Route path="/fee-status" element={<FeeStatus />} />
-        <Route path="/reexam" element={<ReExam />} />
-        <Route path="/result" element={<Result />} />
-        <Route path="/scholarship" element={<Scholarship />} />
-        <Route path="/grievance" element={<Grievance />} />
-        <Route path="/leave" element={<Leave />} />
-        <Route path="/wallet" element={<WalletPage />} />
+      <StudentProvider>
+        {shouldHaveNavbar && <Navbar />}
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/log-in" element={<LogIn />} />
 
-        <Route path="/superAdmin" element={<SuperAdmin />} />
-        <Route path="/doc-admin" element={<DocumentVerificationAdmin />} />
-        <Route path="/marks-admin" element={<MarksAdmin />} />
+          <Route path="/admin-register" element={<AdminRegister />} />
+          <Route path="/admin-login" element={<AdminLogin />} />
 
-        <Route path="/admin-grievance" element={<GrievanceList />} />
-        <Route path="/admin-leave" element={<LeaveApproval />} />
-        <Route path="/admin-scholarship/*" element={<HODScholarship />} />
+          <Route path="/student-detail" element={<Dashboard />} />
+          <Route path="/admission/*" element={<Admission />} />
+          <Route path="/fee-status" element={<FeeStatus />} />
+          <Route path="/reexam" element={<ReExam />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/scholarship" element={<Scholarship />} />
+          <Route path="/grievance" element={<Grievance />} />
+          <Route path="/leave" element={<Leave />} />
+          <Route path="/wallet" element={<WalletPage />} />
 
-        <Route path="/accountant-admin" element={<AddMoney />} />
+          <Route path="/superAdmin" element={<SuperAdmin />} />
+          <Route path="/doc-admin" element={<DocumentVerificationAdmin />} />
+          <Route path="/marks-admin" element={<MarksAdmin />} />
 
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-    </StudentProvider>
+          <Route path="/admin-grievance" element={<GrievanceList />} />
+          <Route path="/admin-leave" element={<LeaveApproval />} />
+          <Route path="/admin-scholarship/*" element={<HODScholarship />} />
+
+          <Route path="/accountant-admin" element={<AddMoney />} />
+
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </StudentProvider>
     </>
   );
 };
