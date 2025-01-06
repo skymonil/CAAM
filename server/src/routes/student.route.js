81e3e58456acd71ab6e43f0fdb7cb4008a336011
Chirag-varu <chiragvaru03@gmail.com>
2025-01-06 14:28:44 +0530

    minor change

--- server/src/routes/student.route.js
+++ server/src/routes/student.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { fillDetails } from "../controllers/student.controller.js";
+// import { authenticateStudent } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.post('/fill-details',fillDetails);
+router.post('/fill-details', fillDetails);
 
 export default router;
